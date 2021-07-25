eafbdf0310b3e32312eb1e1d81c314ad1903c4ae
AnYing <anyingiit@aliyun.com>
2021-07-25 14:18:41 +0800

    fix: 使用eslint进行格式化, 根据修复建议修复完成

--- src/models/xmlHelper.ts
+++ src/models/xmlHelper.ts
@@ -1,53 +1,62 @@
-export class XmlHelper {
-  private xmlObj: Document = new Object as Document
-  private designsDB: Element = new Object as Element
+export default class XmlHelper {
+  private xmlObj: Document = new Document()
+
+  private designsDB: Element = new Element()
+
   constructor(xmlStr: string) {
-    this.parserXml(xmlStr)
-    this.initDesignsDB()
+    this.parserXml(xmlStr);
+    this.initDesignsDB();
   }
+
   private parserXml(xmlStr: string) {
-    let parser = new DOMParser()
-    this.xmlObj = parser.parseFromString(xmlStr, 'text/xml')
+    const parser = new DOMParser();
+    this.xmlObj = parser.parseFromString(xmlStr, 'text/xml');
   }
+
   private initDesignsDB() {
-    let obj = this.xmlObj.getElementsByTagName('DESIGNS_DB')
-    for (let i = 0; i < obj.length; i++) {
+    const obj = this.xmlObj.getElementsByTagName('DESIGNS_DB');
+    for (let i = 0; i < obj.length; i += 1) {
       if (obj[i].tagName === 'DESIGNS_DB') {
-        for (let j = 0; j < obj[i].children.length; j++) {
+        for (let j = 0; j < obj[i].children.length; j += 1) {
           if (obj[i].children[j].tagName === 'VARIABLES') {
-            this.designsDB = obj[i].children[j]
+            this.designsDB = obj[i].children[j];
           }
         }
       }
     }
   }
+
   public getXmlObj(): Document {
-    return this.xmlObj
+    return this.xmlObj;
   }
+
   public getDesignsDB(): Element {
-    return this.designsDB
+    return this.designsDB;
   }
+
   public getXmlStr(): string {
-    let xmlSer = new XMLSerializer()
-    return xmlSer.serializeToString(this.xmlObj)
+    const xmlSer = new XMLSerializer();
+    return xmlSer.serializeToString(this.xmlObj);
   }
+
   public getDesignsDBVar(varName: string): Element | undefined {
-    for (let i = 0; i < this.designsDB.children.length; i++) {
+    for (let i = 0; i < this.designsDB.children.length; i += 1) {
       if (this.designsDB.children[i].getAttribute('name') === varName) {
-        return this.designsDB.children[i]
+        return this.designsDB.children[i];
       }
     }
-    return undefined
+    return undefined;
   }
+
   public getDesignsDBVars(varNames: Array<string>): Array<Element> {
-    let result: Array<Element> = []
-    for (let i = 0; i < this.designsDB.children.length; i++) {
-      varNames.forEach(value => {
+    const result: Array<Element> = [];
+    for (let i = 0; i < this.designsDB.children.length; i += 1) {
+      varNames.forEach((value) => {
         if (this.designsDB.children[i].getAttribute('name') === value) {
-          result.push(this.designsDB.children[i])
+          result.push(this.designsDB.children[i]);
         }
-      })
+      });
     }
-    return result
+    return result;
   }
 }
