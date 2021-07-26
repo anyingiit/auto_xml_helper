2afbc087896c2c26f6c3c10ed681e635f3c1926c
AnYing <anyingiit@aliyun.com>
2021-07-26 12:36:01 +0800

    fix: 1. 使用eslint进行格式化 2. 使用es6语法导入了 url, path, fs.readFile 3. 测试ipcMain的方法中, 使用异步回复消息代替同步回复

--- src/background.ts
+++ src/background.ts
@@ -1,16 +1,19 @@
-'use strict'
+import {
+  app, protocol, BrowserWindow, ipcMain,
+} from 'electron';
+import { createProtocol } from 'vue-cli-plugin-electron-builder/lib';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import installExtension, { VUEJS3_DEVTOOLS } from 'electron-devtools-installer';
+// import url from 'url';
+import path from 'path';
+import { readFile } from 'fs';
 
-import { app, protocol, BrowserWindow, ipcMain, session } from 'electron'
-import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
-import installExtension, { VUEJS3_DEVTOOLS } from 'electron-devtools-installer'
-const isDevelopment = process.env.NODE_ENV !== 'production'
-const url = require("url");
-const path = require("path");
-const fs = require('fs')
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
-  { scheme: 'app', privileges: { secure: true, standard: true } }
-])
+  { scheme: 'app', privileges: { secure: true, standard: true } },
+]);
 
 async function createWindow() {
   // Create the browser window.
@@ -20,21 +23,23 @@ async function createWindow() {
     webPreferences: {
 
       // Use pluginOptions.nodeIntegration, leave this alone
-      // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
+      // See
+      //  nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration
+      //  for more info
       nodeIntegration: (process.env
         .ELECTRON_NODE_INTEGRATION as unknown) as boolean,
-      contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION
-    }
-  })
+      contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION,
+    },
+  });
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
-    await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL as string)
-    if (!process.env.IS_TEST) win.webContents.openDevTools()
+    await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL as string);
+    if (!process.env.IS_TEST) win.webContents.openDevTools();
   } else {
-    createProtocol('app')
+    createProtocol('app');
     // Load the index.html when not in development
-    win.loadURL('app://./index.html')
+    win.loadURL('app://./index.html');
   }
 }
 
@@ -43,15 +48,15 @@ app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
-})
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -60,56 +65,55 @@ app.on('ready', async () => {
   if (isDevelopment && !process.env.IS_TEST) {
     // Install Vue Devtools
     try {
-      await installExtension(VUEJS3_DEVTOOLS)
+      await installExtension(VUEJS3_DEVTOOLS);
     } catch (e) {
-      console.error('Vue Devtools failed to install:', e.toString())
+      console.error('Vue Devtools failed to install:', e.toString());
     }
     // installExtension(VUEJS3_DEVTOOLS)
     // .then((name) => console.log(`Added Extension:  ${name}`))
     // .catch((err) => console.log('An error occurred: ', err));
   }
-  createWindow()
-})
+  createWindow();
+});
 
 // Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
   if (process.platform === 'win32') {
     process.on('message', (data) => {
       if (data === 'graceful-exit') {
-        app.quit()
+        app.quit();
       }
-    })
+    });
   } else {
     process.on('SIGTERM', () => {
-      app.quit()
-    })
+      app.quit();
+    });
   }
 }
 
-
-
 ipcMain.on('asynchronous-message', (event, arg) => {
-  console.log(arg) // prints "ping"
-  event.reply('asynchronous-reply', 'pong')
-})
+  console.log(arg); // prints "ping"
+  event.reply('asynchronous-reply', 'pong');
+});
 
 ipcMain.on('synchronous-message', (event, arg) => {
-  console.log(arg) // prints "ping"
-  event.returnValue = 'pong'
-})
+  console.log(arg); // prints "ping"
+  // event.returnValue = 'pong';
+  event.sender.send('pong');
+});
 
 ipcMain.on('getXmlConfig', (event, arg) => {
   // console.log("in")
-  const targetPath = path.join(__dirname, "../data/m.xml")
+  const targetPath = path.join(__dirname, '../data/m.xml');
   // console.log(targetPath)
-  fs.readFile(targetPath, "utf-8", (err: Error, data: string) => {
+  readFile(targetPath, 'utf-8', (err, data) => {
     if (err) {
       // event.sender.send('asynchronous-reply', "读取失败");
-      console.log("读取失败")
+      console.log('读取失败');
     } else {
       // event.sender.send('asynchronous-reply', data);
-      console.log("读取成功:\n\t" + data.slice(0, 20) + "...")
-      event.reply('getXmlConfig-replay', data)
+      console.log(`读取成功:\n\t${data.slice(0, 20)}...`);
+      event.reply('getXmlConfig-replay', data);
     }
-  })
-})
+  });
+});
